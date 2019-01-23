e9b42f38aa57aa6e0d9f7ea2d733d159f975e10b
Peerawit-SC <peerawit.sc@gmail.com>
2019-01-23 21:57:01 +0700

    add

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title></title>
+    <title>raspberry pi's ip</title>
 </head>
 <body>
-<h1>Raspberry Pi's IP</h1>
+<h1>Raspberry Pi's IP :</h1>
 <form method="POST" action="/ip.php">
-<input type="text" name="ip">
+<input type="text" name="ip" placeholder="192.168.x.xxx">
 <button type="submit">Submit</button>
 </form>
 </body>
